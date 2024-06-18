ce43ec6ae4f38c7037d5fdd4f7e940de6a94195a
vipin <vipin945324@gmail.com>
2024-06-18 23:50:46 +0530

    Login Design Completed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // circleImageView  dependency
+    implementation (libs.circleimageview)
 }
